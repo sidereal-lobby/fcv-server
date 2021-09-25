6634ea94cdd703f0ff2a038335049e38b2e4e707
Ryan Laws <ryanlaws@gmail.com>
2021-09-25 09:26:30 -0700

    Initialize repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple WebSocket Echo Server
---
+++ src/echo-server.js
@@ -0,0 +1,59 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+console.log('started %s', 1)
+
+wss.on("connection", function connection(ws, request, client) {
+  console.log(`#args: ${arguments.length}`);
+
+  // WebSocket
+  //console.log(
+  //  `connection:`, Object.keys(ws), 
+  //  `sender: `, Object.keys(ws._sender),
+  //  `receiver: `, Object.keys(ws._receiver)
+  //);
+
+  // request
+  //console.log(`request:`, Object.keys(request));
+
+  // client
+  //console.log(`client:`, client);
+
+
+  // ws object is for a specific client
+  ws.on("message", function incoming(message) {
+    //console.log("oh hi: %s", message);
+    // TODO: 
+    // - implement different message types
+    //   - GREET
+    //   - SC
+    //   - LUA
+    //   - MESSAGE
+    //   - SYNC
+    //     - server: grab epoch before send
+    //     - server: send message
+    //     - server: grab epoch after send
+    //     - client: grab receive epoch on client
+    //     - client: send 1
+    //     - client: grab post-send epoch on client
+    //     - client: send 2
+    //     - server: grab epoch after receive
+    //     - server: send back to client
+    //     - calculate round trip, delays
+    // - read from /stonks/ and /sparkles/
+    //   - plug into fsevents
+    //   - filenames
+    //     - something.lua
+    //     - something.sc
+    //     - something.txt
+    // or do something reasonable like headers or wahtever
+    let i = 0;
+    wss.clients.forEach((client) => { 
+      //console.log(`client #${i}: `, Object.keys(client));
+      client.send(`hi client ${++i}! did you say "${message.toString()}"? neat!`);
+    });
+    console.log(`# clients: ` + i);
+    //console.log(`clients: `, );
+  });
+});
