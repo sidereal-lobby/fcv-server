224065442f4716602f39fa3f9e8b98231172a21f
Ryan Laws <ryanlaws@gmail.com>
2021-09-27 08:15:18 -0700

    Clean up
    Add connection map
    Clean up messages for commands
    Clean up structure

--- src/echo-server.js
+++ src/echo-server.js
@@ -1,35 +1,42 @@
 import { WebSocketServer } from "ws";
 
+let id = 0;
+const connMap = new Map();
+
 const wss = new WebSocketServer({ port: 8080 });
 
-console.log('started %s', 1)
+console.log('Started server');
+
+const blastClients = (msg) => {
+  let i = 0;
+  msg = msg.toString();
+
+  console.log(`Sending message to clients: "${msg}"`);
+  wss.clients.forEach((client) => { 
+    client.send(msg);
+  });
+
+  console.log(`Sent to ${i} clients.`);
+}
 
 wss.on("connection", function connection(ws, request, client) {
-  console.log(`#args: ${arguments.length}`);
-
-  // WebSocket
-  //console.log(
-  //  `connection:`, Object.keys(ws), 
-  //  `sender: `, Object.keys(ws._sender),
-  //  `receiver: `, Object.keys(ws._receiver)
-  //);
-
-  // request
-  //console.log(`request:`, Object.keys(request));
-
-  // client
-  //console.log(`client:`, client);
-
+  connMap.set(ws, ++id);
+  console.log(`New client: Client #${id}`);
+  ws.send(`MSG\nWelcome, Client #${id}!`);
+  ws.send(`LUA\ngraphics.title = 'Connected!'`);
+  //console.log(`#args: ${arguments.length}`);
 
   // ws object is for a specific client
-  ws.on("message", function incoming(message) {
+  ws.on("message", blastClients);
+    //function incoming(message) {
+    //blastClients(message);
     //console.log("oh hi: %s", message);
     // TODO: 
     // - implement different message types
     //   - GREET
-    //   - SC
-    //   - LUA
-    //   - MESSAGE
+    //   x SC
+    //   x LUA
+    //   x MSG
     //   - SYNC
     //     - server: grab epoch before send
     //     - server: send message
@@ -48,17 +55,5 @@ wss.on("connection", function connection(ws, request, client) {
     //     - something.sc
     //     - something.txt
     // or do something reasonable like headers or wahtever
-    let i = 0;
-    //const msg = `did someone say "${message.toString()}"? neat!`;
-    const msg = message.toString();
-
-    console.log(`sending message to clients: "${msg}"`);
-    wss.clients.forEach((client) => { 
-      //console.log(`client #${i}: `, Object.keys(client));
-      //client.send(`hi client ${++i}! ${msg}`);
-      client.send(msg);
-    });
-    console.log(`# clients: ` + i);
-    //console.log(`clients: `, );
-  });
+  //});
 });
